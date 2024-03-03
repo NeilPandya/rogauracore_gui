b0eddbe2787936205a321c5af00f6edec2f10ecd
Neil Pandya <neil@neilpandya.com>
2024-03-03 15:59:19 +0100

    Added a command to restart upower.service to preserve function keys for keyboard lighting brightness. 	modified:   main.cpp 	modified:   src/resource.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include <cstdlib>
+#include <gtkmm/application.h>
 
 #include "src/roggui.h"
 
 int main(int argc, char** argv)
 {
-	Glib::RefPtr<Gtk::Application> app = Gtk::Application::create("rogauracore.gui");	
-	RogGui gui;
+    Glib::RefPtr<Gtk::Application> app = Gtk::Application::create("rogauracore.gui");	
+    RogGui gui;
 
-	return app->run(*gui.window_main);
+    // Connect the "hide" signal to a lambda function
+    gui.window_main->signal_hide().connect([&]()
+    {
+        // Run the command to restart upower.service using system()
+        std::system("sudo systemctl restart upower.service");
+    });
+
+    return app->run(*gui.window_main);
 }
