356e6e4e9c45dd886c76cc3f4aeb61dc27555a6d
Neil Pandya <neil@neilpandya.com>
2024-03-11 22:16:01 +0100

    Added bash script and command to restart upower.service.

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
-#include <cstdlib>
 #include <gtkmm/application.h>
+#include <glibmm/miscutils.h>
 
 #include "src/roggui.h"
 
 int main(int argc, char** argv)
 {
-    Glib::RefPtr<Gtk::Application> app = Gtk::Application::create("rogauracore.gui");	
+    Glib::RefPtr<Gtk::Application> app = Gtk::Application::create("rogauracore.gui");  
     RogGui gui;
 
-    // Connect the "hide" signal to a lambda function
-    gui.window_main->signal_hide().connect([&]()
+    gui.window_main->signal_delete_event().connect([&](GdkEventAny* event)
     {
-        // Run the command to restart upower.service using system()
-        std::system("sudo systemctl restart upower.service");
+        // Run the shell script
+        Glib::spawn_command_line_async("./restart_upower.sh");
+
+        // Returning false allows the default behavior (destroy the window).
+        return false;
     });
 
     return app->run(*gui.window_main);
